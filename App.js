7e7fcc0d48de6a79eed2377553911070de1e7afb
RinkaGI <labatanightcore@gmail.com>
2024-03-09 12:51:53 +0100

    download button try

--- App.js
+++ App.js
@@ -3,10 +3,16 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Provider as PaperProvider } from "react-native-paper";
 import DownloadForm from "./DownloadForm";
 
+import ytdl from 'ytdl-core';
+import RNFS from 'react-native-fs';
+
 const App = () => {
   const handleDownload = (url) => {
     // LÃ³gica para descargar el video
     console.log("Descargando video desde:", url);
+
+    // ytdl(url)
+    //   .pipe(RNFS.createWriteStream('video.mp4'));
   };
 
   return (
